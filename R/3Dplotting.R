87f9f23ac71e620d7850523b8bf953ab94bd4911
mreichMPI-BGC <mreichstein@bgc-jena.mpg.de>
2021-12-13 10:26:11 +0100

    More examples

--- R/3Dplotting.R
+++ R/3Dplotting.R
@@ -57,6 +57,8 @@ values2rgb  <- function(values, pal=palette(), valRange=range(values, na.rm = TR
 #'
 #' @examples ## Plot a colored surface plot of volcano data set
 #' rs_surface(elmat=volcano, coloring=volcano, pal=terrain.colors(50), zscale=5)
+#' ## Change e.g. coloring, so that it reflects the difference from the mean height
+#' rs_surface(elmat=volcano, coloring=abs(volcano - mean(volcano)), pal=pal_MR$divTurbo, zscale=5)
 rs_surface  <- function(elmat = volcano, coloring=volcano, img_overlay1=NULL, img_overlay2=NULL, water = FALSE,
                         wateralpha=0.5, alpha_over=0.75,
                         texture="bw", zscale=250, shadowintens=0.8, shadowReachFactor=1, windowsize=c(1920, 1080), zoom=0.5,
